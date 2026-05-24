d9e0dd76f3a2b44c89054155dfe2d949494469b5
AMohammedShafeek <amdshafeek7@gmail.com>
2026-05-24 16:35:46 +0530

    feat: data preprocessing

---
+++ prepare.py
@@ -0,0 +1,90 @@
+# pyrefly: ignore [missing-import]
+import librosa
+
+# pyrefly: ignore [missing-import]
+from datasets import load_dataset
+
+from transformers import WhisperProcessor
+
+print("Loading processor...")
+
+processor = WhisperProcessor.from_pretrained(
+    "openai/whisper-medium"
+)
+
+# =========================
+# LOAD DATASET
+# =========================
+
+print("\nLoading dataset...")
+
+dataset = load_dataset(
+    "csv",
+    data_files="dataset/metadata.csv"
+)
+
+dataset = dataset["train"]
+
+print("\nDataset Loaded!")
+
+print(dataset)
+
+# =========================
+# PREPROCESS FUNCTION
+# =========================
+
+def prepare_sample(sample):
+
+    # Full audio path
+    audio_path = (
+        f"dataset/audio/{sample['file']}"
+    )
+
+    # Load audio
+    audio, sample_rate = librosa.load(
+        audio_path,
+        sr=16000
+    )
+
+    # Audio → Features
+    inputs = processor(
+        audio,
+        sampling_rate=16000,
+        return_tensors="pt"
+    )
+
+    # Text → Token IDs
+    labels = processor.tokenizer(
+        sample["text"]
+    ).input_ids
+
+    return {
+        "input_features":
+            inputs.input_features[0],
+
+        "labels":
+            labels
+    }
+
+# =========================
+# PROCESS DATASET
+# =========================
+
+print("\nProcessing dataset...")
+
+processed_dataset = dataset.map(
+    prepare_sample
+)
+
+print("\nDataset Processed Successfully!")
+
+# =========================
+# SHOW FIRST SAMPLE
+# =========================
+
+print("\nFirst Processed Sample:\n")
+
+print(processed_dataset[0]["labels"])
+
+print("\nKeys:")
+print(processed_dataset[0].keys())
